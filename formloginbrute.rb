81310930e45247107a1d845885abed1729ad6735
krishpranav <krisna.pranav@gmail.com>
2021-06-16 12:35:24 +0530

    [+] usernames and passwords function [+]

--- formloginbrute.rb
+++ formloginbrute.rb
@@ -9,4 +9,13 @@ module LoginFormBruterForcer
 
     module_function
 
+    def usernames_and_passwords(user_file="users.txt", password_file="pass.txt")
+        Enumerator.new do |enum|
+            File.opn(users_file, "r").each do |user|
+                File.open(password_file, "r") do |password|
+                    enum.yield user, password
+                end
+            end
+        end
+    end
     

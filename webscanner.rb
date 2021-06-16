8d4d2369a973f4a9a1ff72e6fe446df6407f0622
krishpranav <krisna.pranav@gmail.com>
2021-06-16 12:50:42 +0530

    [+] requires [+]

---
+++ webscanner.rb
@@ -0,0 +1,16 @@
+# requires
+require "csv"
+require "net/http"
+require "net/http/persistent"
+require "net/https"
+require "nmap/program"
+require "nmap/xml"
+require "optparse"
+require "ostruct"
+require "text-table"
+require "uri"
+require "thread"
+require 'yaml'
+require 'logger'
+require 'sqlite3'
+require 'fileutils'

f2bee41f21aa6d4ce8bb4fddf89a291df19c365c
krishpranav <krisna.pranav@gmail.com>
2021-06-16 13:30:25 +0530

    [+] self delegate methods [+]

--- webscanner.rb
+++ webscanner.rb
@@ -31,3 +31,12 @@ class MultiDelegaotr
         @targets = targets
     end
     
+    def self.delegate(*methods)
+        methods.each do |m|
+            define_method(m) do |*args|
+                @targets.map { |t| t.send(m, *args)}
+            end
+        end
+        self
+    end
+    

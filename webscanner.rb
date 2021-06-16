2efdaa6f911a6c2d80bd5fd3bb6f3faff8cd5012
krishpranav <krisna.pranav@gmail.com>
2021-06-16 12:52:23 +0530

    [+] string class [+]

--- webscanner.rb
+++ webscanner.rb
@@ -18,3 +18,10 @@ require 'fileutils'
 require File.dirname(File.realpath(__FILE__)) + '/formloginbrute.rb'
 
 VERSION = '1.0'
+
+class String
+    def red; colorize(self, "\e[1m\e[31m"); end
+    def green; colorize(self, "\e[1m\e[32m"); end
+    def bold; colorize(self, "\e[1m"); end
+    def colorize(text, color_code)  "#{color_code}#{text}\e[0m" end
+end
